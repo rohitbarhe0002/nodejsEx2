ee6938d8b9e4b1e94d2e02dd40808bc493fdab4c
Rohit barche <rohitbarche99@gmail.com>
2022-01-19 14:44:26 +0530

    event module

--- fsasync/WebServer/server.js
+++ fsasync/WebServer/server.js
@@ -4,6 +4,6 @@ const server = http.createServer(()=>(req,res) =>{
     res.end('hello from server')
 } )
 
-server.listen(8006, "127.0.0.1",() =>{
-    console.log('listining to the port 8000');
+server.listen(8001, "127.0.0.2",() =>{
+    console.log('listining to the port 80001');
 })
---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+//fs.writeFileSync("read.txt","hello");
+// fs.writeFileSync("read.txt","Rohit");
+// fs.appendFileSync("read.txt","i am");
+// var content = fs.readFileSync('read.txt', 'utf8');
+// console.log("this",content);
+
+
+
+fs.unlink('NodejsEx2/indexe.js');
